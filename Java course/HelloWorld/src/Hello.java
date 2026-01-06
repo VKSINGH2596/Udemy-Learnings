4c6028be9ad0fce049c9029aa3e3c8fe6e7bb764
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-01-06 13:11:36 +0530

    update Hello class with some stuff

--- Java course/HelloWorld/src/Hello.java
+++ Java course/HelloWorld/src/Hello.java
@@ -1,6 +1,9 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Vaibhav");
-        System.out.println("Hello World");
+        
+        boolean isAlien = false;
+        if (isAlien == false)
+            System.out.println("It is not an alien!");
     }
 }
