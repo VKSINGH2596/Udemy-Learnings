0e9e45d6f5d2eba84ede069d1442e3672562aa1a
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-01-26 16:52:14 +0530

    Add logical operator examples and conditional statements
    - Added comments for logical AND and OR operators
    - Added newValue variable with equality comparison example
    - Added boolean isCar variable with negation operator demonstration
    - Included error handling example with comparison operators

--- Java course/HelloWorld/src/Hello.java
+++ Java course/HelloWorld/src/Hello.java
@@ -18,12 +18,22 @@ public class Hello {
         }
 
         int secondTopScore = 60;
-        if ((topScore > secondTopScore) && (topScore < 100)) {
+        if ((topScore > secondTopScore) && (topScore < 100)) {      // logical AND operator
             System.out.println("Greater than second top score and less than 100");
         }
 
-        if ((topScore > 90) || (secondTopScore <= 90)) {
+        if ((topScore > 90) || (secondTopScore <= 90)) {        // logical OR operator
             System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue = 50;
+        if (newValue == 50) {       // using single equal sign for assignment, double equal sign for comparison
+            System.out.println("This is an Error");
+        }
+
+        boolean isCar = false;
+        if (isCar) {            // negation operator
+            System.out.println("This is not supposed to happen");
         }
     }
 }
