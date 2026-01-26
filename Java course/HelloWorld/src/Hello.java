1ecac56afaebeb9a33f1b352053fef63eef599aa
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-01-26 16:41:32 +0530

    update Hello clase with Logical AND & OR operator example

--- Java course/HelloWorld/src/Hello.java
+++ Java course/HelloWorld/src/Hello.java
@@ -3,7 +3,27 @@ public class Hello {
         System.out.println("Hello, Vaibhav");
         
         boolean isAlien = false;
-        if (isAlien == false)
+        if (isAlien == false) {
             System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 100;
+        if (topScore < 100) {
+            System.out.println("You got less than high score!");
+        }
+
+        if (topScore <= 100) {
+            System.out.println("You got equal or under high score");
+        }
+
+        int secondTopScore = 60;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
