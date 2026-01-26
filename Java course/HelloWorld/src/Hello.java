d3367609351a2958a0a856dbb9243b5af234741e
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-01-26 17:04:16 +0530

    Add ternary operator example and Challenge 1 solution
    - Added makeOfCar string variable with ternary operator demonstration
    - Implemented Challenge 1 with double precision calculations
    - Added modulus operator usage for remainder calculation
    - Used ternary operator for boolean assignment and conditional logic
    - Added debugging output statements for value tracking

--- Java course/HelloWorld/src/Hello.java
+++ Java course/HelloWorld/src/Hello.java
@@ -35,5 +35,24 @@ public class Hello {
         if (isCar) {            // negation operator
             System.out.println("This is not supposed to happen");
         }
+
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;   // ternary operator
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        // Challenge 1
+        double firstValue = 20.00d;
+        double secondValue = 80.00d;
+        double totalValue = (firstValue + secondValue) * 100.00d;
+        System.out.println("\nTotal Value: " + totalValue);
+        double remainder = totalValue % 40.00d;
+        System.out.println("Remainder: " + remainder);
+        boolean isNoRemainder = (remainder == 0) ? true : false;
+        System.out.println("Is there no remainder? " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
     }
 }
