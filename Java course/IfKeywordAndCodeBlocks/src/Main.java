82e2c294a329a1c094a88031138260fdefbb367c
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-02-26 15:16:18 +0530

    Java method creation and test

--- Java course/IfKeywordAndCodeBlocks/src/Main.java
+++ Java course/IfKeywordAndCodeBlocks/src/Main.java
@@ -1,16 +1,32 @@
 public class Main {
     public static void main(String[] args) {
         boolean gameOver = true;
-        int score = 500;
-        int levelCompleted = 6;
+        int score = 900;
+        int levelCompleted = 5;
         int bonus = 100;
 
-        if (score > 600) {
-            System.out.println("Your score was greater than 600");
-        } else if (score == 600) {
-            System.out.println("Your score was 600");
-        } else {
-            System.out.println("Your score was less than 600");
+        int finalScore = score;
+        
+        calculateScore(gameOver, score, levelCompleted, bonus);
+
+        score = 10000;
+        levelCompleted = 8;
+        bonus = 200;
+        finalScore = score;
+        
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your new final score is " + finalScore);
+        }
+    }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        
+        int finalScore = score;
+        
+        if (gameOver) {
+            finalScore += (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
         }
     }
 }
