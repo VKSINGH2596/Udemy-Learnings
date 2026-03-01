b41f6797bd51736df50b2f6e093f4a0d43c3dd90
VKSINGH2596 <vaibhav.singh2596@gmail.com>
2026-03-01 18:54:26 +0530

    NEW parts for Java method

--- Java course/IfKeywordAndCodeBlocks/src/Main.java
+++ Java course/IfKeywordAndCodeBlocks/src/Main.java
@@ -5,19 +5,13 @@ public class Main {
         int levelCompleted = 5;
         int bonus = 100;
 
-        int finalScore = score;
-        
         calculateScore(gameOver, score, levelCompleted, bonus);
 
         score = 10000;
         levelCompleted = 8;
         bonus = 200;
-        finalScore = score;
         
-        if (gameOver) {
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your new final score is " + finalScore);
-        }
+        calculateScore(gameOver, score, levelCompleted, bonus);
     }
 
     public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
